7aca3c01ff20a1b0aeecbda30e6e6e17dbfc65db
Abiseban <31163831+Abiseban147@users.noreply.github.com>
2020-04-20 11:25:49 +0530

    example-react-app completed

---
+++ example-rect-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import RadioMap from './RadioMap'
+import "react-svg-map/lib/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <RadioMap/>
+    </div>
+  );
+}
+
+export default App;
